6c0a13aabe4ea951f01b0b4581b53451ec871454
María Ruiz <mrp990305@gmail.com>
2024-02-02 17:32:51 +0100

    user, student, company migration

---
+++ database/migrations/2024_02_02_161154_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table-> bigInteger('user_id');
+            $table->text('address');
+            $table->text('cv_link');
+            $table->boolean('accept')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
