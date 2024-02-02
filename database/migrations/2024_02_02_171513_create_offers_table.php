40c642572533660198a3a7ea12bec67f3c6f4b71
Arantxa Gandia <aragansan@alu.edu.gva.es>
2024-02-02 18:24:08 +0100

    tabla oferta

---
+++ database/migrations/2024_02_02_171513_create_offers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('description',200);
+            $table->string('duration',50);
+            $table->string('responsibleName',100);
+            $table->boolean('inscriptionMethod');
+            $table->boolean('status');
+            $table->string('cif',9);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offers');
+    }
+};
