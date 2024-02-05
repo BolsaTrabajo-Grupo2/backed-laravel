7b81b0cd5d549117f57292ce0b79059616d6a9c6
Arantxa Gandia <aragansan@alu.edu.gva.es>
2024-02-05 19:31:06 +0100

    roles responsable y admin

---
+++ app/Http/Middleware/EnsureIsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user()->rol != 'ADM') {
+
+            abort(403, 'Acció no autoritzada.');
+        }
+
+        return $next($request);
+    }
+}
