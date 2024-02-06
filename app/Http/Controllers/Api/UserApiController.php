c6585a6cb4e2a84c450f782ebcb8e4e70003bcde
María Ruiz <mrp990305@gmail.com>
2024-02-06 15:51:03 +0100

    register

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserApiController extends Controller
 {
-    //
+
+    public function register(UserRequest $request)
+    {
+        $user = new User();
+        $user->name = $request->get('name');
+        $user->surname = $request->get('surname');
+        $user->email = $request->get('email');
+        $user->password = Hash::make($request->get('password'));
+        $user->rol = $request->get('rol');
+
+        $user->save();
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
 }
