3672e0cad408b49539593c448cb34d4db34d520a
LucasJR13 <lucjuariq@alu.edu.gva.es>
2024-02-06 15:55:51 +0100

    Api Company terminada

--- app/Http/Controllers/Api/CompanyApiController.php
+++ app/Http/Controllers/Api/CompanyApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CompanyRequest;
 use App\Http\Resources\CompanyCollection;
 use App\Http\Resources\CompanyResource;
 use App\Models\Company;
@@ -19,9 +20,30 @@ class CompanyApiController extends Controller
     {
         return new CompanyResource($company);
     }
-
-    public function store()
+    public function store(CompanyRequest $request)
     {
+        $company = new Company();
+        $company->CIF = $request->get('CIF');
+        $company->user_id = $request->get('idUser');
+        $company->address = $request->get('address');
+        $company->phone = $request->get('phone');
+        $company->web = $request->get('web');
+        $company->save();
+        return new CompanyResource($company);
+    }
 
+    public function update(CompanyRequest $request, $id)
+    {
+        $company = Company::findOrFail($id);
+
+        $company->CIF = $request->get('CIF');
+        $company->user_id = $request->get('idUser');
+        $company->address = $request->get('address');
+        $company->phone = $request->get('phone');
+        $company->web = $request->get('web');
+
+        $company->save();
+
+        return new CompanyResource($company);
     }
 }
