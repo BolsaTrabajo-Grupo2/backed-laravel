a2d6d827372ccfe45a74f18f7ccd5694b9e3bd94
María Ruiz <mrp990305@gmail.com>
2024-02-08 15:31:24 +0100

    rol

--- app/Http/Middleware/EnsureUserRol.php
+++ app/Http/Middleware/EnsureUserRol.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\UnauthorizedException;
 use Illuminate\Auth\AuthenticationException;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,20 +16,15 @@ class EnsureUserRol
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, ...$roles)
+    public function handle(Request $request, Closure $next, ... $roles): Response
     {
-        $user = $request->user();
-
-        if (!$user) {
-            throw new UnauthorizedException('Unauthenticated.');
-        }
-
-        foreach ($roles as $role) {
-            if ($user->hasRole($role)) {
-                return $next($request);
+        if (Auth::check()) {
+            foreach ($roles as $rol) {
+                if (Auth::user()->hasRole($rol)) {
+                    return $next($request);
+                }
             }
         }
-
         throw new UnauthorizedException('Forbidden.');
     }
 }
