54fb7632138beb21fa807e1cc63ded56d34de514
María Ruiz <mrp990305@gmail.com>
2024-02-08 15:30:09 +0100

    api arreglada?

--- routes/api.php
+++ routes/api.php
@@ -24,20 +24,23 @@ Route::middleware('rol:STU,COMP')->group(function () {
     Route::get('user/profile ',[\App\Http\Controllers\Api\UserApiController::class, 'show']);
     Route::put('user/profile/update/{id}', [\App\Http\Controllers\Api\UserApiController::class, 'update']);
 });
-Route::middleware('rol:STU,COMP,ADMIN,RESP')->group(function (){
+Route::middleware('rol:STU,COMP,ADM,RESP')->group(function (){
     Route::get('/active/{id}',[\App\Http\Controllers\Api\StudentApiController::class,'active']);
 });
 //la ruta de abajo es para pillar el estudiante
 Route::get('student/{id}', [\App\Http\Controllers\Api\StudentApiController::class, 'getStudent']);
 //la ruta de abajo es para pillar los ciclos de un estudiante
 Route::get('studentCicles/{id}',[\App\Http\Controllers\Api\StudentApiController::class,'getCycleByStudent']);
-Route::apiResource('company',\App\Http\Controllers\Api\CompanyApiController::class)->middleware('rol:ADMIN,RESP,COMP');
-Route::delete('student/delete/{id}',[\App\Http\Controllers\Api\UserApiController::class, 'delete'])->middleware('rol:ADMIN,RESP,STU');
-Route::apiResource('responsible',\App\Http\Controllers\Api\UserApiController::class)->middleware('rol:RESP');
-Route::get('validate/{id}',[\App\Http\Controllers\Api\OfferApiController::class, 'validate'])->middleware('rol:ADMIN,RESP');
-Route::apiResource('students', \App\Http\Controllers\Api\StudentApiController::class)->middleware('rol:ADMIN,STU');
-Route::apiResource('users', \App\Http\Controllers\Api\UserApiController::class)->middleware('rol:ADMIN');
-Route::apiResource('offers', \App\Http\Controllers\Api\OfferApiController::class)->middleware('rol:ADMIN,COMP');
 Route::get('cycles', [CycleApiController::class, 'getAll']);
 //la ruta para comprobar el email
 Route::get('checkEmail/{email}',[\App\Http\Controllers\Api\UserApiController::class,'checkEmail']);
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::apiResource('company',\App\Http\Controllers\Api\CompanyApiController::class)->middleware('rol:ADM,RESP,COMP');
+    Route::delete('student/delete/{id}',[\App\Http\Controllers\Api\UserApiController::class, 'delete'])->middleware('rol:ADM,RESP,STU');
+    Route::apiResource('responsible',\App\Http\Controllers\Api\UserApiController::class)->middleware('rol:RESP');
+    Route::get('validate/{id}',[\App\Http\Controllers\Api\OfferApiController::class, 'validate'])->middleware('rol:ADM,RESP');
+    Route::apiResource('students', \App\Http\Controllers\Api\StudentApiController::class)->middleware('rol:ADM,STU');
+    Route::apiResource('users', \App\Http\Controllers\Api\UserApiController::class)->middleware('rol:ADM');
+    Route::apiResource('offers', \App\Http\Controllers\Api\OfferApiController::class)->middleware('rol:ADM,COMP');
+});
