2f37a10f93b4e1143947ce21aacfc02037f5ce9c
María Ruiz <mrp990305@gmail.com>
2024-02-09 19:25:12 +0100

    student update request

--- app/Http/Controllers/Api/StudentApiController.php
+++ app/Http/Controllers/Api/StudentApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StudentRequest;
+use App\Http\Requests\StudentUpdateRquest;
 use App\Http\Resources\StudentCollection;
 use App\Http\Resources\StudentResource;
 use App\Models\Student;
@@ -50,7 +51,7 @@ class StudentApiController extends Controller
         return response()->json(['token' => $token], 201);
     }
 
-    public function update(StudentRequest $studentRequest, $id){
+    public function update(StudentUpdateRquest $studentRequest, $id){
         $userApi = new UserApiController();
         $userResponse = $userApi->update($studentRequest,$id);
         $student = Student::where('id_user', $id)->firstOrFail();
@@ -101,6 +102,11 @@ class StudentApiController extends Controller
         }
 
         foreach ($user->getAttributes() as $key => $value) {
+            // Omitir la contraseña y el correo electrónico
+            if ($key === 'password' || $key === 'email') {
+                continue;
+            }
+            // Agregar los demás atributos al objeto mergedData
             $mergedData->$key = $value ?? '';
         }
 
