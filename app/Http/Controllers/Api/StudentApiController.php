05671c16c61b3aa00c28aea7f81615bcec6caecc
Arantxa Gandia <aragansan@alu.edu.gva.es>
2024-02-12 18:52:47 +0100

    [MOD] Redirigue a la vista tars activar

--- app/Http/Controllers/Api/StudentApiController.php
+++ app/Http/Controllers/Api/StudentApiController.php
@@ -95,7 +95,7 @@ class StudentApiController extends Controller
         $student->save();
         $user = User::findOrFail($student->id_user);
         $user->notify(new ActivedNotification());
-        return 'Hola';
+        return view('users.actived');
     }
     public function getStudent($id) {
         $user = User::findOrFail($id);
---
+++ resources/views/users/actived.blade.php
@@ -0,0 +1,1 @@
+<h1>Ya has activado tu cuenta, ahora podras disfrutar de la bolsa de trabajo de batoi</h1>
