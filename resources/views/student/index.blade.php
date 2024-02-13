944744f049c84469cebc6d30d3b45677cec5e620
LucasJR13 <lucjuariq@alu.edu.gva.es>
2024-02-13 16:40:10 +0100

    estadisticas students

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -11,6 +11,7 @@
                     <th>Nombre</th>
                     <th>Apellidos</th>
                     <th>Email</th>
+                    <th>Ofertas Aplicadas</th>
                     <th>Acciones</th>
                 </tr>
                 </thead>
@@ -20,6 +21,7 @@
                         <td>{{ $student->name }}</td>
                         <td>{{ $student->surname }}</td>
                         <td>{{ $student->email }}</td>
+                        <td>{{ $student->applies_count }}</td>
                         <td>
                             <a href="{{ route('student.edit', $student->id) }}" class="btn btn-primary">Editar</a>
                             <form action="{{ route('student.destroy', $student->id) }}" method="POST" style="display: inline-block;">
@@ -33,5 +35,6 @@
                 </tbody>
             </table>
         </div>
+        {{ $students->links() }}
     </div>
 @endsection
