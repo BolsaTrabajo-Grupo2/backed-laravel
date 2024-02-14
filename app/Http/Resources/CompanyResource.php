5f5a3fd53f1a062fe070dcaf56e6a32ef188969d
Arantxa Gandia <aragansan@alu.edu.gva.es>
2024-02-14 16:20:44 +0100

    pagina en vue

--- app/Http/Resources/CompanyResource.php
+++ app/Http/Resources/CompanyResource.php
@@ -17,7 +17,7 @@ class CompanyResource extends JsonResource
         return [
             'idUsuario' => $this->user->id,
             'CIF' => $this->CIF,
-            'companyName' => $this->companyName,
+            'companyName' => $this->company_name,
             'direccion' => $this->address,
             'CP' => $this->CP,
             'telefono' => $this->phone,
